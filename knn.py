47964951fc645e67a5756b8f6106bbf652a26e87
Koushik Gavini <koushik.gavini@sjsu.edu>
2017-10-09 11:55:38 -0700

    used nn-2 to find

--- knn.py
+++ knn.py
@@ -1,5 +1,45 @@
-import numpy as np
-from sklearn import preprocessing, cross_validation, neighbors
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np  
+import csv
+from sklearn.preprocessing import StandardScaler
 
-df = pd.read_csv()
+
+# # read flash.dat to a list of lists
+# datContent = [i.strip().split() for i in open("train.dat").readlines()]
+
+# # write it as a new CSV file
+# with open("train.csv", "wb") as f:
+#     writer = csv.writer(f)
+#     writer.writerows(datContent)
+# def your_func(row):
+#     return row['Sentiments'] / row['Review']
+
+# columns_to_keep = ['Sentiments', 'Review']
+# dataframe = pd.read_csv("train.csv", usecols=columns_to_keep)
+# dataframe['new_column'] = dataframe.apply(your_func, axis=1)
+
+# print dataframe
+# df = pd.read_fwf('train.dat', header=None, 
+#         widths=[2, int(1e15)], names=['Sentiments', 'Review'])
+
+# print(df)
+# scaler = StandardScaler()
+# scaled_features = scaler.transform(df.drop('Sentiments',axis=1))
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+
+# read in the dataset
+df = pd.read_csv(
+    filepath_or_buffer='train.dat', 
+    header=None, 
+    #sep=','
+    )
+# extract the vectors from the Pandas data file
+X = df.iloc[:,1:].values
+
+# standardise the data
+X_std = StandardScaler().fit_transform(X)
+print(X_std)
---
+++ knn2.py
@@ -0,0 +1,134 @@
+import numpy as np
+import pandas as pd
+import scipy.sparse as sp
+from numpy.linalg import norm
+from collections import Counter, defaultdict
+from scipy.sparse import csr_matrix
+
+# df = pd.read_fwf('train.dat', header=None, 
+#         widths=[2, int(1e10)], names=['Sentiment', 'Review'])
+
+# print(df)
+
+import csv
+
+with open('train.dat','r') as f:
+    df = pd.DataFrame(l.rstrip().split() for l in f)
+
+# print(df)
+vals = df.ix[:,:].values
+names = [n[0][2:] for n in vals]
+cls = [n[0][0] for n in vals]
+
+def cmer(name, c=3):
+    r""" Given a name and parameter c, return the vector of c-mers associated with the name
+    """
+    name = name.lower()
+    if len(name) < c:
+        return [name]
+    v = []
+    for i in range(len(name)-c+1):
+        v.append(name[i:(i+c)])
+    return v
+def build_matrix(docs):
+    r""" Build sparse matrix from a list of documents, 
+    each of which is a list of word/terms in the document.  
+    """
+    nrows = len(docs)
+    idx = {}
+    tid = 0
+    nnz = 0
+    for d in docs:
+        nnz += len(set(d))
+        for w in d:
+            if w not in idx:
+                idx[w] = tid
+                tid += 1
+    ncols = len(idx)
+        
+    # set up memory
+    ind = np.zeros(nnz, dtype=np.int)
+    val = np.zeros(nnz, dtype=np.double)
+    ptr = np.zeros(nrows+1, dtype=np.int)
+    i = 0  # document ID / row counter
+    n = 0  # non-zero counter
+    # transfer values
+    for d in docs:
+        cnt = Counter(d)
+        keys = list(k for k,_ in cnt.most_common())
+        l = len(keys)
+        for j,k in enumerate(keys):
+            ind[j+n] = idx[k]
+            val[j+n] = cnt[k]
+        ptr[i+1] = ptr[i] + l
+        n += l
+        i += 1
+            
+    mat = csr_matrix((val, ind, ptr), shape=(nrows, ncols), dtype=np.double)
+    mat.sort_indices()
+    
+    return mat
+
+def csr_info(mat, name="", non_empy=False):
+    r""" Print out info about this CSR matrix. If non_empy, 
+    report number of non-empty rows and cols as well
+    """
+    if non_empy:
+        print("%s [nrows %d (%d non-empty), ncols %d (%d non-empty), nnz %d]" % (
+                name, mat.shape[0], 
+                sum(1 if mat.indptr[i+1] > mat.indptr[i] else 0 
+                for i in range(mat.shape[0])), 
+                mat.shape[1], len(np.unique(mat.indices)), 
+                len(mat.data)))
+    else:
+        print( "%s [nrows %d, ncols %d, nnz %d]" % (name, 
+                mat.shape[0], mat.shape[1], len(mat.data)) )
+
+def csr_l2normalize(mat, copy=False, **kargs):
+    r""" Normalize the rows of a CSR matrix by their L-2 norm. 
+    If copy is True, returns a copy of the normalized matrix.
+    """
+    if copy is True:
+        mat = mat.copy()
+    nrows = mat.shape[0]
+    nnz = mat.nnz
+    ind, val, ptr = mat.indices, mat.data, mat.indptr
+    # normalize
+    for i in range(nrows):
+        rsum = 0.0    
+        for j in range(ptr[i], ptr[i+1]):
+            rsum += val[j]**2
+        if rsum == 0.0:
+            continue  # do not normalize empty rows
+        rsum = 1.0/np.sqrt(rsum)
+        for j in range(ptr[i], ptr[i+1]):
+            val[j] *= rsum
+            
+    if copy is True:
+        return mat
+        
+def namesToMatrix(names, c):
+    docs = [cmer(n, c) for n in names]
+    return build_matrix(docs)
+# From Activity nn-2-Sol
+#We'll now define a function to search for the top- kk  neighbors for a given name (one of the objects the dataset), where proximity is computed via cosine similarity.
+def findNeighborsForName(name, c=1, k=1):
+    # first, find the document for the given name
+    id = -1
+    for i in range(len(names)):
+        if names[i] == name:
+            id = i
+            break
+    if id == -1:
+        print("Name %s not found." % name)
+        return []
+    # now, compute similarities of name's vector against all other name vectors
+    mat = namesToMatrix(names, c)
+    csr_l2normalize(mat)
+    x = mat[id,:]
+    dots = x.dot(mat.T)
+    dots[0,id] = -1 # invalidate self-similarity
+    sims = list(zip(dots.indices, dots.data))
+    sims.sort(key=lambda x: x[1], reverse=True)
+    return [names[s[0]] for s in sims[:k] if s[1] > 0 ]
+print(names)
